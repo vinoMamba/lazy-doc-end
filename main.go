b61c5071e8210478e7d6fc878fb7a4a90ad8f42c
wangxin <vino0908@outlook.com>
2023-11-16 13:54:47 +0800

    restart

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-
-	"github.com/gin-gonic/gin"
-
-	"github.com/vinoMamba/lazy-doc-end/bootstrap"
-	btsConfig "github.com/vinoMamba/lazy-doc-end/config"
-	"github.com/vinoMamba/lazy-doc-end/pkg/config"
 )
 
-func init() {
-	btsConfig.Initialize()
-}
-
 func main() {
-	var env string
-	flag.StringVar(&env, "env", "", "load the .env file")
-	flag.Parse()
-	config.InitConfig(env)
-
-	port := config.Get("app.port")
-	fmt.Println(port)
-
-	r := gin.New()
-	bootstrap.SetupRoute(r)
-
-	err := r.Run(":3000")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	fmt.Println("Hello, World!")
 }
