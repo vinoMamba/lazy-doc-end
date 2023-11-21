ce6e7620a8ab2476f2b28ca13fe0d496b5feab6a
vino <vino0908@outlook.com>
2023-11-21 23:35:02 +0800

    change the status code

--- handler/user.go
+++ handler/user.go
@@ -36,7 +36,7 @@ func userRegister(c *gin.Context) {
 
 	if ok := utils.VerifyEmail(body.Username); !ok {
 		log.Errorln("email verify failed")
-		c.JSON(http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"code":    1,
 			"message": "email verify failed",
 			"data":    nil,
@@ -47,7 +47,7 @@ func userRegister(c *gin.Context) {
 	u, _ := db.GetUserByEmail(c, body.Username)
 	if u.ID != 0 {
 		log.WithField("email", u.Email).Warnln("the email has been registered")
-		c.JSON(http.StatusConflict, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"code":    1,
 			"message": "The email has been registered",
 			"data":    nil,
@@ -57,13 +57,14 @@ func userRegister(c *gin.Context) {
 
 	if ok := utils.VerifyPassword(body.Password, body.ConfirmPassword); !ok {
 		log.Errorln("password verify failed")
-		c.JSON(http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"code":    1,
 			"message": "password verify failed",
 			"data":    nil,
 		})
 		return
 	}
+
 	hashedPassword, err := utils.HashPassword(body.Password)
 	if err != nil {
 		log.WithError(err).Errorln("Hash password failed")
